acd2644849d0494c02fe17e28d380da9c57f230a
unknown <kateplum99@gmail.com>
2022-11-08 21:54:01 +0100

    second commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// const button = document.getElementById("toggleButton");
+// const buttonClose = document.getElementById("toggleButtonClose");
+// const width = document.getElementById("offCanvas");
+
+
+
+// button.addEventListener("click", function (e) {
+//   width.style.width = "300px";
+
+// });
+
+// buttonClose.addEventListener("click", function (e) {
+//   width.style.width = "0";
+// });
